29a761efd41b5c4729d4b59e5a9ef78c2f5033fb
Chohanwool <josephchome888@gmail.com>
2025-05-13 23:15:51 +0900

    [data] 퀴즈 더미 데이터 추가 및 모델 생성성

---
+++ lib/model/quiz_question.dart
@@ -0,0 +1,14 @@
+class QuizQuestion {
+  final String text;
+  final List<String> answers;
+
+  const QuizQuestion({required this.text, required this.answers});
+
+  List<String> get shuffledAnswers {
+    final shuffledResult = List.of(answers);
+    shuffledResult.shuffle(); 
+    // final 변수인 shuffledResult에 shuffle 을 사용해서 리스트 요소의 
+    // 순서가 변하는것은 새로운 인스턴스를 참조하는것이 아니기 때문에 가능하다
+    return shuffledResult;
+  }
+}
