6424a544a50b1c0f3a5df1d027c4fdc5aff2ce16
Chohanwool <josephchome888@gmail.com>
2025-05-13 22:14:02 +0900

    [feat] 앱 화면 구성성

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(const QuizApp());
-}
-
-class QuizApp extends StatelessWidget {
-  const QuizApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 59, 33, 104),
-                const Color.fromARGB(255, 102, 57, 182),
-              ],
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    );
-  }
+  runApp(const Quiz());
 }
